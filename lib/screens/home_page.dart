1db154888de7d62e60fa1a31251bada90a2fbe15
ESUM BRANDON <esumbrandon074@gmail.com>
2024-01-29 00:24:34 +0100

    Fetchn from server

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/screens/add_todo.dart';
+import 'package:http/http.dart' as http;
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -9,11 +10,26 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchTodo();
+  }
+
   void navigateTo() {
     final route = MaterialPageRoute(
       builder: (context) => const AddTodo(),
     );
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    const url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    print(response.statusCode);
+    print(response.body);
   }
 
   @override
