59c03c0ea2775dbc2dda980234ba0468c23c6acf
ESUM BRANDON <esumbrandon074@gmail.com>
2024-01-29 12:36:51 +0100

    data fetch done and displayed on device

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/screens/home_page.dart';
+import 'package:todo_app/screens/todol_list.dart';
 
 void main() {
   runApp(const MyApp());
